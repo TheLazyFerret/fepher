7ede0241e70908f0e2b7a9df5949c691ecb94b50
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-05-02 09:30:03 +0100

    Add multiple methods

--- src/gopher/gopher.cpp
+++ src/gopher/gopher.cpp
@@ -1,18 +1,72 @@
-/// Gopher protocol auxiliar methods implementation.
+/// Gopher protocol  methods implementation.
 
 #include "./gopher.hpp"
-#include <string_view>
 
-constexpr std::string_view TERMINATOR = "\r\n";
+#include <cassert>
+#include <expected>
+#include <string>
+#include <string_view>
+#include <system_error>
+
+#include <limits.h>
+#include <stdlib.h>
+
+using namespace gopher;
 
 /// Check if the string end in the terminator.
-bool gopher::string_end_in_terminator(std::string_view e) noexcept {
-  if (e.size() < 2) {
+bool gopher::string_end_in_terminator(std::string_view selector) noexcept {
+  if (selector.size() < 2) {
+    return false;
+  } else if (selector[selector.size() - 2] == TERMINATOR[0] && selector[selector.size() - 1] == TERMINATOR[1]) {
+    return true;
+  } else {
     return false;
   }
+}
 
-  if (e[e.size() - 2] == TERMINATOR.at(0) && e[e.size() - 1] == TERMINATOR.at(1)) {
-    return true;
+/// Return the received selector without the terminator (\r\n).
+std::string gopher::get_selector_without_terminator(std::string_view selector) noexcept {
+  assert(selector.size() >= 2);
+  assert(selector[selector.size() - 2] == TERMINATOR[0] && selector[selector.size() - 1] == TERMINATOR[1]);
+  return std::string(selector.substr(0, selector.size() - 2));
+}
+
+/// Get the path from the selector. This function assumes the path parameter doesn't end with "\r\n".
+/// Basically just append '/' where is needed.
+std::string gopher::get_path(const std::string& path, const std::string& fileserver) noexcept {
+  assert(!gopher::string_end_in_terminator(path));
+  assert(fileserver.size() > 0 && fileserver.back() == '/'); // The fileserver path should end with '/'.
+  if (path.size() == 0) {
+    return fileserver;
+  } else if (path[0] == '/') {
+    return fileserver + path.substr(1); // Ignores the path '/'.
+  } else {
+    return fileserver + path;
   }
-  return false;
+}
+
+/// Return the real path, also checking if the path is correct.
+/// Basically a safer wrapper around realpath().
+std::expected<std::string, std::error_code> gopher::get_real_path(const std::string& path) noexcept {
+  char result[PATH_MAX]{};
+  if (realpath(path.c_str(), result) == nullptr) {
+    return std::unexpected(std::error_code(errno, std::generic_category()));
+  }
+  return {result};
+}
+
+/// Returns true if the path (assumes it is a complete and valid path, see get_real_path()) is inside the fileserver.
+/// The path must not have any type of symbolic/hard links.
+/// Also assumes fileserver_path is ended in '/'
+bool gopher::is_path_safe(std::string_view path, std::string_view fileserver_path) noexcept {
+  assert(fileserver_path.back() == '/');
+  if (fileserver_path.size() > path.size()) {
+    return false;
+  }
+  for (std::size_t n = 0; n < fileserver_path.size(); ++n) {
+    if (path[n] != fileserver_path[n]) {
+      return false;
+    }
+  }
+  return true;
 }
--- src/gopher/gopher.hpp
+++ src/gopher/gopher.hpp
@@ -1,12 +1,21 @@
-/// Gopher protocol auxiliar methods.
+/// Gopher protocol functions.
+/// In this "module" is where most of the gopher protocol logic is.
 
 #pragma once
 
-// #include <string>
+#include <expected>
+#include <string>
 #include <string_view>
+#include <system_error>
 
 namespace gopher {
 
-bool string_end_in_terminator(std::string_view e) noexcept;
+constexpr std::string_view TERMINATOR = "\r\n";
 
-} // namespace ghoper
+bool string_end_in_terminator(std::string_view) noexcept;
+std::string get_selector_without_terminator(std::string_view) noexcept;
+std::string get_path(const std::string& path, const std::string& fileserver) noexcept;
+std::expected<std::string, std::error_code> get_real_path(const std::string&) noexcept;
+bool is_path_safe(std::string_view path, std::string_view fileserver) noexcept;
+
+} // namespace gopher
